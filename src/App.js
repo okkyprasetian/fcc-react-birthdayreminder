ffc74baa2581b0d988458c852db8a657dc7ca986
okkyprasetian <okkyprasetian@gmail.com>
2021-11-04 09:40:50 +0700

    initialize data state in App.js

--- src/App.js
+++ src/App.js
@@ -1,9 +1,39 @@
 import Header from './components/Header'
 import Lists from './components/Lists'
 import Footer from './components/Footer'
-import './App.css';
+import './App.css'
+import { useState } from 'react'
 
 function App() {
+
+  const [lists, setLists] = useState([
+    {
+      id: 1,
+      name: "Anthonio Baroteli",
+      age: 63
+    },
+    {
+      id: 2,
+      name: "Nannie Jennie",
+      age: 22
+    },
+    {
+      id: 3,
+      name: "Maradona",
+      age: 11
+    },
+    {
+      id: 4,
+      name: "Matthew Anderson",
+      age: 25
+    },
+    {
+      id: 5,
+      name: "Olivia Ceres",
+      age: 20
+    }
+  ])
+
   return (
     <div className="App">
       <div id="main">
